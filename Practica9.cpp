9087b07ddee337d8579db4a484886a2d805558be
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-06-05 08:13:19 -0400

    Practica 9: arrays en C++

---
+++ Practica9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vec[5]={12,45,67,11,78};
+
+    for(int i=0;i<5;i++)
+    {
+        cout<<vec[i]<<" ";
+    }
+
+    return 0;
+}
